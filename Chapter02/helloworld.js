e70c68e9b7386c86ae5ddd20938cbc0fb5e7230e
philipmatusiak <97487605+philipmatusiak@users.noreply.github.com>
2022-07-26 20:06:37 -0400

    Update helloworld.js

--- Chapter02/helloworld.js
+++ Chapter02/helloworld.js
@@ -1,12 +1,6 @@
 var http = require("http");
-
 http.createServer(function (request, response) {
-
    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    response.end('Hello World\n');
-   
+   response.end('Hello World\n');   
 }).listen(3000);
-
-// Console will print the message
 console.log('Server running');
